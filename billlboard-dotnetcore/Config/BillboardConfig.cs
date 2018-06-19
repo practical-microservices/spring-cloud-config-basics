e30df060a66402184378fd6714512f2d6684eca4
Andrew Stakhov <astakhov@pivotal.io>
2018-06-19 10:05:44 -0400

    billboard .NET implementation

---
+++ billlboard-dotnetcore/Config/BillboardConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Billboard.Config
+{
+    public class BillboardConfig
+    {
+        public string Message { get; set; } = "value was not read from config server";
+    }
+}
---
+++ billlboard-dotnetcore/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Billboard.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Steeltoe.Management.Endpoint.Health;
+
+namespace Billboard
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+        }
+
+        // register services into container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<BillboardConfig>(_configuration);
+            services.AddHealthActuator(_configuration);
+            services.AddMvc();
+        }
+
+        // activate services & setup middleware
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHealthActuator();
+            app.UseStaticFiles();
+            app.UseMvc();
+            
+        }
+    }
+}
